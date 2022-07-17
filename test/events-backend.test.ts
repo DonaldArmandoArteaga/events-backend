eac6e2b58385268dadf39f451cdbb72a82d54afc
Donald Armando Torres Arteaga <donald.torres@avista.co>
2022-07-17 16:53:48 -0500

    scaffolding

---
+++ test/events-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventsBackend from '../lib/events-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/events-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventsBackend.EventsBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
