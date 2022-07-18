9ab190ef7370a02146f2b0654f3da208f2bb3361
Donald Armando Torres Arteaga <donald.torres@avista.co>
2022-07-17 19:43:29 -0500

    add health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import * as express from "express"
 const app: Express = express()
 const port = 8080;
 
-app.get('/', (_: Request, res: Response) => {
-    res.json({})
+app.get('/healthCheck', (_: Request, res: Response) => {
+    res.status(200).send()
 });
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-});
+})
